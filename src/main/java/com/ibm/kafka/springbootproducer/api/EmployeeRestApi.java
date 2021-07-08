fb6511f16f58adeeab6ffd4f07a83164c4df6c68
Nikhil Lagishetty <nikhil.lagishetty@ibm.com>
2021-07-08 15:40:49 +0530

    code refactor

--- src/main/java/com/ibm/kafka/springbootproducer/api/EmployeeRestApi.java
+++ src/main/java/com/ibm/kafka/springbootproducer/api/EmployeeRestApi.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
 
 @Slf4j
 @RestController
